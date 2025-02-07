6ec615151affe21e7197509a3808cef607f0df59
mantinasagar <sagarmantina11@gmail.com>
2025-02-07 21:59:18 +0530

    fixed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,7 +7,8 @@ import AdminPage from "./pages/AdminPage";
 import CustomerPage from "./pages/CustomerPage";
 import AgentPage from "./pages/AgentPage";
 import HomePage from "./pages/HomePage";
-const backend_url = process.env.REACT_APP_BACKEND_URL;
+const backend_url = process.env.REACT_APP_BACKEND_URL || "http://localhost:5000";
+console.log(backend_url);
 function App() {
   const [role, setRole] = useState(null);
   const [loading, setLoading] = useState(true);
