6635d1b76824635c194f024bf3df94d0aec28079
Mantina Sagar <123088264+SagarMantina@users.noreply.github.com>
2025-02-07 23:42:47 +0530

    Updated fixed LoginPage.jsx

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { useNavigate } from "react-router-dom";
+
 import axios from "axios";
 import AOS from "aos";
 import "aos/dist/aos.css";
