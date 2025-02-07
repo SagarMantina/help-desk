606752458c29a311aa0509f867304e34c38eebc0
Mantina Sagar <123088264+SagarMantina@users.noreply.github.com>
2025-02-07 22:06:10 +0530

    Update AdminPage.jsx

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import AOS from "aos";
 import "aos/dist/aos.css";
+const backend_url = "https://help-desk-bfld.onrender.com" || "http://localhost:5000";
 const AdminPage = () => {
   const [tickets, setTickets] = useState([]);
   const [note, setNote] = useState("");
